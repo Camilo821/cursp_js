adec68f4444563034790033ccaeeb1400173b087
= <=>
2022-07-06 15:50:26 -0500

    cuarto ejercicio

--- index.js
+++ index.js
@@ -13,3 +13,14 @@
 // La diferencia entre var y let es que let es una variable que solo existe en el bloque de codigo donde fue declarada
 // var es una variable que existe en todo el codigo
 // const es una variable que no puede cambiar su valor
+let nombre = "Camilo";
+let apellido = "Arboleda";
+let estudiante = nombre + " " + apellido;
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+let estudianteLength = estudiante.length;
+let first_letter = estudiante[0];
+let last_letter = estudiante[estudiante.length - 1];
+let estudiante_sin_espacios = estudiante.replace(" ", "");
+let nombre_in_estudiante = estudiante.includes(nombre)
+console.log(nombre_in_estudiante)
