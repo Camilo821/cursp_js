557fa95dde756148a4157e97de0613965c404fff
= <=>
2022-07-20 16:20:32 -0500

    septimo ejercicio

--- index.js
+++ index.js
@@ -13,26 +13,8 @@
 // La diferencia entre var y let es que let es una variable que solo existe en el bloque de codigo donde fue declarada
 // var es una variable que existe en todo el codigo
 // const es una variable que no puede cambiar su valor
-lista =  ["Sal", "Azúcar", "Café", "Atún", "Cerveza"];
-lista.push("Aceite de Girasol");
-lista.pop();
-peliculas = [{
-    titulo: "Avengers: Endgame",
-    director: "Anthony Russo",
-    anio: new Date(2019, 04, 05)
-},
-{
-    titulo: "Avengers: Infinity War",
-    director: "Anthony Russo",
-    anio: new Date(2018, 05, 25)
-},
-{
-    titulo: "Piratas del Caribe: La maldición del Perla Negra",
-    director: "Gore Verbinski",
-    anio: new Date(2003, 08, 01)
-}]
-peliculasp2010 = peliculas.filter(valor => valor.anio.getFullYear() > 2010);
-directores = peliculas.map(valor => valor.director);
-titulos = peliculas.map(valor => valor.titulo);
-titulos_directores = titulos.concat(directores);
-titulos_directores_map = peliculas.map(valor => valor.titulo + " - " + valor.director);
+let lista = ["Camilo", "Diana", "Carlos", "Leidy", "Camilo"];
+let set = new Set(lista);
+console.log(set);
+set.add("Javascript");
+console.log(set);
