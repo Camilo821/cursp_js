612453033b0c649174203da7f9f9ce70c8d97aca
= <=>
2022-07-20 16:40:38 -0500

    noveno ejercicio

--- index.js
+++ index.js
@@ -13,18 +13,9 @@
 // La diferencia entre var y let es que let es una variable que solo existe en el bloque de codigo donde fue declarada
 // var es una variable que existe en todo el codigo
 // const es una variable que no puede cambiar su valor
-let datos = {
-    nombre: "Camilo",
-    apellido: "Arboleda",
-    edad: 14,
-    altura: 1.60
-}
-let datos_lista = [
-    { ...datos },
-    {nombre: "Juan Diego", apellido: "Jimenez", edad: 14, altura: 1.85},
-    {nombre: "Juan", apellido: "Perez", edad: 15, altura: 1.70},
-]
-let datos_ordenados = datos_lista.sort((a, b) => a.edad - b.edad);
-console.log(datos_ordenados);
-console.log(datos_lista);
-console.log(datos)
+let date = date.now();
+let fecha_nacimiento = new Date(2007, 07, 21);
+let bool = fecha_nacimiento.getTime() < date.getTime();
+let bool2 = fecha_nacimiento.getDay()
+let bool3 = fecha_nacimiento.getMonth() + 1
+let bool4 = fecha_nacimiento.getFullYear()
